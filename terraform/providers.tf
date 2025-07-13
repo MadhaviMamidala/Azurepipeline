ae42f5c1bc8a02a3c892503dbbe19210324eeaea
Madhavi Mamidala <madhu12mamidala@gmail.com>
2025-07-13 00:31:09 -0700

    added tfstate file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,6 +9,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstateaccyw951kbq"  
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
